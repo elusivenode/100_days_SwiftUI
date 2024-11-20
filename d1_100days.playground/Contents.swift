eb55d497578529918b599fededf8e40061078699
Hamish MacDonald <hamish.macdonald@neds.com>
2024-11-20 16:03:54 +1000

    day 1

---
+++ d1_100days.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+//constant
+let name = "Hamish"
+
+print(name)
+
+let s = "this is a very long string.  It's so very long isn't it."
+let c = s.count
+print(c)
+
+print(s.hasPrefix("this"))
+print(s.hasPrefix("blah"))
+
+let n = 120
+print(n.isMultiple(of: 60))
+print(n.isMultiple(of: 61))
+
+
